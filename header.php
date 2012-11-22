3a7738e3bc43ce5e0fcc6e1100018861affa28df
Sami Keijonen <sami.keijonen@foxnet.fi>
2012-11-22 17:28:01 +0200

    IE8 image support added.

--- header.php
+++ header.php
@@ -13,7 +13,11 @@
  */
 ?>
 <!DOCTYPE html>
+<!--[if IE 7 ]> <html class="ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8 ]> <html class="ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
 <html <?php language_attributes(); ?>>
+<!--<![endif]-->
 <head>
 <meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 <title><?php hybrid_document_title(); ?></title>
