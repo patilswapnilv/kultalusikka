f133c9f4749c8683fa4c52d3f6d63e535ddbe5b7
Sami Keijonen <sami.keijonen@foxnet.fi>
2012-12-07 14:23:35 +0200

    Content-image template added.

--- admin/functions-admin.php
+++ admin/functions-admin.php
@@ -10,8 +10,11 @@
 /* Theme setting page setup. */
 add_action( 'admin_menu', 'kultalusikka_theme_admin_setup' );
 
-/* Theme activate license. */
+/* Theme deactivate license. */
 add_action( 'admin_init', 'kultalusikka_theme_activate_license' );
+
+/* Theme activate license. */
+add_action( 'admin_init', 'kultalusikka_theme_deactivate_license' );
 
 function kultalusikka_theme_admin_setup() {
     
@@ -106,9 +109,11 @@ function kultalusikka_theme_meta_box_licence_key() {
 			<td>
 			<?php if( $kultalusikka_status !== false && $kultalusikka_status == 'valid' ) { ?>
 				<span style="color:green;"><?php _e( 'Active' ); ?></span>
+				<?php wp_nonce_field( 'kultalusikka_licence_nonce', 'kultalusikka_licence_nonce' ); ?>
+				<input type="submit" class="button-secondary" name="kultalusikka_theme_license_deactivate" value="<?php _e( 'Deactivate License', 'kultalusikka' ); ?>" />
 				<?php } else {
 					wp_nonce_field( 'kultalusikka_licence_nonce', 'kultalusikka_licence_nonce' ); ?>
-					<input type="submit" class="button-secondary" name="kultalusikka_theme_license_activate" value="<?php _e('Activate License'); ?>" />
+					<input type="submit" class="button-secondary" name="kultalusikka_theme_license_activate" value="<?php _e( 'Activate License', 'kultalusikka' ); ?>" />
 					
 				<?php } ?>
 			</td>
@@ -213,7 +218,7 @@ function kultalusikka_theme_activate_license() {
 			'item_name' => urlencode( KULTALUSIKKA_SL_THEME_NAME ) 
 		);
 		
-		$response = wp_remote_get( add_query_arg( $api_params, KULTALUSIKKA_SL_STORE_URL ) );
+		$response = wp_remote_get( add_query_arg( $api_params, KULTALUSIKKA_SL_STORE_URL ), array( 'timeout' => 15, 'sslverify' => false ) );
 
 		if ( is_wp_error( $response ) )
 			return false;
@@ -223,6 +228,37 @@ function kultalusikka_theme_activate_license() {
 		// $license_data->license will be either "active" or "inactive"
 
 		update_option( 'kultalusikka_license_key_status', $license_data->license );
+
+	}
+}
+
+/* Deactivate theme license. */
+function kultalusikka_theme_deactivate_license() {
+
+	if( isset( $_POST['kultalusikka_theme_license_deactivate'] ) ) { 
+	 	if( ! check_admin_referer( 'kultalusikka_licence_nonce', 'kultalusikka_licence_nonce' ) ) 	
+			return; // get out if we didn't click the Deactivate button
+
+		global $wp_version;
+
+		$license = trim( hybrid_get_setting( 'kultalusikka_licence_key' ) );
+				
+		$api_params = array( 
+			'edd_action' => 'deactivate_license', 
+			'license' => $license, 
+			'item_name' => urlencode( KULTALUSIKKA_SL_THEME_NAME ) 
+		);
+		
+		$response = wp_remote_get( add_query_arg( $api_params, KULTALUSIKKA_SL_STORE_URL ), array( 'timeout' => 15, 'sslverify' => false ) );
+
+		if ( is_wp_error( $response ) )
+			return false;
+
+		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+		
+		/* $license_data->license will be either "deactivated" or "failed". */
+		if( $license_data->license == 'deactivated' )
+			delete_option( 'kultalusikka_license_key_status' );
 
 	}
 }
