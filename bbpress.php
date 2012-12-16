108cbb2cbe73eec27cc2dffe4e51e470ed2ecae5
Sami Keijonen <sami.keijonen@foxnet.fi>
2012-12-16 12:41:57 +0200

    Better bbpress template.

--- bbpress.php
+++ bbpress.php
@@ -24,7 +24,32 @@ get_header(); // Loads the header.php template. ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 				
-				<?php get_template_part( 'content', get_post_type() ); ?>
+					<?php do_atomic( 'before_entry' ); // kultalusikka_before_entry ?>
+
+					<article id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
+
+						<?php do_atomic( 'open_entry' ); // kultalusikka_open_entry ?>
+	
+						<header class="entry-header">
+							<?php if ( is_singular() && is_main_query() ) {
+								echo apply_atomic_shortcode( 'entry_title', '[entry-title]' );
+							}
+							else {
+								echo apply_atomic_shortcode( 'entry_title', '[entry-title tag="h2"]' ); 
+							} 
+							?>
+						</header><!-- .entry-header -->
+		
+						<div class="entry-content">
+							<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'kultalusikka' ) ); ?>
+							<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', 'kultalusikka' ), 'after' => '</p>' ) ); ?>
+						</div><!-- .entry-content -->
+
+						<?php do_atomic( 'close_entry' ); // kultalusikka_close_entry ?>
+
+					</article><!-- .hentry -->
+					
+					<?php do_atomic( 'after_entry' ); // kultalusikka_after_entry ?>
 
 					<?php if ( is_singular() ) { ?>
 
